7d618595c783c4f65683f96047ea4c6c4a8d5a31
teiyuueki <tu.ue.et@gmail.com>
2016-03-30 23:22:22 +0900

    moved

---
+++ app/src/main/java/inducesmile/com/androidstaggeredgridlayoutmanager/MainActivity.java
@@ -0,0 +1,100 @@
+package inducesmile.com.androidstaggeredgridlayoutmanager;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private StaggeredGridLayoutManager gaggeredGridLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        gaggeredGridLayoutManager = new StaggeredGridLayoutManager(2, 1);
+        recyclerView.setLayoutManager(gaggeredGridLayoutManager);
+
+        List<ItemObjects> gaggeredList = getListItemData();
+
+        SolventRecyclerViewAdapter rcAdapter = new SolventRecyclerViewAdapter(MainActivity.this, gaggeredList);
+        recyclerView.setAdapter(rcAdapter);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private List<ItemObjects> getListItemData(){
+        List<ItemObjects> listViewItems = new ArrayList<ItemObjects>();
+        listViewItems.add(new ItemObjects("Alkane", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ethane", R.drawable.two));
+        listViewItems.add(new ItemObjects("Alkyne", R.drawable.three));
+        listViewItems.add(new ItemObjects("Benzene", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amide", R.drawable.one));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        listViewItems.add(new ItemObjects("Amino Acid", R.drawable.two));
+        listViewItems.add(new ItemObjects("Phenol", R.drawable.three));
+        listViewItems.add(new ItemObjects("Carbonxylic", R.drawable.four));
+        listViewItems.add(new ItemObjects("Nitril", R.drawable.one));
+        listViewItems.add(new ItemObjects("Ether", R.drawable.two));
+        listViewItems.add(new ItemObjects("Ester", R.drawable.three));
+        listViewItems.add(new ItemObjects("Alcohol", R.drawable.four));
+        return listViewItems;
+    }
+}
